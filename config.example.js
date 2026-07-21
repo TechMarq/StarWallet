a879bdcdf39fb6d05c773d92e72c42d1e450ae30
TechMark Soluções & Tecnologia <115334886+TechMarq@users.noreply.github.com>
2026-07-20 23:34:22 -0300

    START PROJECTS 200726-2333
    START PROJECTS 200726-2333

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// Arquivo de configuração de exemplo
+// Renomeie este arquivo para config.js e preencha com as suas credenciais do Supabase.
+
+const SUPABASE_URL = "SUA_URL_DO_SUPABASE";
+const SUPABASE_ANON_KEY = "SUA_CHAVE_ANONIMA_DO_SUPABASE";
---
+++ config.js
@@ -0,0 +1,5 @@
+// Insira aqui suas credenciais do Supabase.
+// Este arquivo é ignorado pelo Git (definido no .gitignore) e não será exposto publicamente.
+
+const SUPABASE_URL = "https://wnjkwwdmfnriinprepyw.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Induamt3d2RtZm5yaWlucHJlcHl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1OTA4ODIsImV4cCI6MjEwMDE2Njg4Mn0.1yLLFWjJSKO4XleQb7XY_B8PlDv9OGCTTj83Si81woc";
